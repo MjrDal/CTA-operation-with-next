22b50370d3c2745e66036a4e3863ba3a21577bd5
MjrDal <mjrdal.39@gmail.com>
2024-04-08 16:22:15 +0200

    add page

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,11 +1,30 @@
+"use server";
+
 import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { PrismaClient } from "@prisma/client";
 
 const DashboardPage = async () => {
   const session = await auth();
+  const prisma = new PrismaClient();
+  const intervention = await prisma.intervention.findMany();
+  const commune = await prisma.communes.findMany();
 
   session?.user.id;
 
-  return <div>{JSON.stringify(session)}</div>;
+  function handleSubmit() {
+    console.log(intervention.length);
+    console.log(commune.length);
+  }
+
+  return (
+    <div>
+      {JSON.stringify(session)}{" "}
+      <div>
+        <Button>creation d&apos;une intervention manuellement</Button>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
---
+++ components/game/list-depart.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const ListDepart = () => {
+  return <div></div>;
+};
