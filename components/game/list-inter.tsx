35f936ddb227ff7cc0971d3e22c1a450aafb065e
MjrDal <mjrdal.39@gmail.com>
2024-04-09 15:53:02 +0200

    add card list inter

--- components/game/list-inter.tsx
+++ components/game/list-inter.tsx
@@ -2,6 +2,7 @@
 
 import { Card } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 import { PrismaClient } from "@prisma/client";
 
 interface Props {}
@@ -14,7 +15,7 @@ const ListInter: React.FC<Props> = async () => {
 
   return (
     <div>
-      <ScrollArea className="h-72 w-96 rounded-md border">
+      <ScrollArea className="h-72 w-[700px] rounded-md border">
         <div className="p-4">
           <h4 className="mb-4 text-sm font-medium leading-none">
             Interventions en cours
@@ -22,13 +23,16 @@ const ListInter: React.FC<Props> = async () => {
           {generation.map((doc) => (
             <div key={doc.id}>
               <Card>
-                <div>
+                <div className=" flex flex-row h-5 items-center space-x-4 text-sm">
                   <div>{doc.numero}</div>
+                  <Separator orientation="vertical" />
                   <div>{doc.denomination}</div>
+                  <Separator orientation="vertical" />
                   <div>
                     {doc.commune} {doc.code}
                   </div>
                 </div>
+                <Separator className="" />
                 <div>
                   <h5>Vehicules en intervention</h5>
                   <div>{doc.vehicules[0].name}</div>
