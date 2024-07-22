45ebe489d62e35ec8acfad3577935d82e77216db
MjrDal <mjrdal.39@gmail.com>
2024-07-22 21:09:05 +0200

    save

--- components/game/card_inter.tsx
+++ components/game/card_inter.tsx
@@ -4,6 +4,9 @@ import { Card } from "@/components/ui/card";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Separator } from "@/components/ui/separator";
 import { PrismaClient } from "@prisma/client";
+import { FaFireFlameCurved } from "react-icons/fa6";
+import { MdLocationOn } from "react-icons/md";
+import { TbClockHour10, TbFiretruck } from "react-icons/tb";
 import { ModaleAjoutVehicules } from "./modale_ajout_vehicules";
 import { ModaleDescription } from "./modale_description";
 import { ModaleMessages } from "./modale_message";
@@ -36,20 +39,39 @@ export const CardInter: React.FC<Props> = async ({ id }) => {
         <DialogTrigger asChild>
           <Card>
             <div className=" flex flex-row h-5 items-center space-x-4 text-sm">
-              <div>{intervention[0].numero}</div>
+              <div className=" p-6">#{intervention[0].numero}</div>
               <Separator orientation="vertical" />
-              <div>{intervention[0].denomination}</div>
+              <div className=" flex flex-row items-center gap-2">
+                <TbClockHour10 className="" />
+                <p>12h15 22/08/2024</p>
+              </div>
               <Separator orientation="vertical" />
-              <div>
-                {intervention[0].commune} {intervention[0].code}
+              <div className=" flex flex-row items-center gap-2">
+                <FaFireFlameCurved />
+                <p>{intervention[0].denomination}</p>
+              </div>
+              <Separator orientation="vertical" />
+              <div className=" flex flex-row items-center gap-2">
+                <MdLocationOn />
+                <p>
+                  {intervention[0].commune} {intervention[0].code}
+                </p>
               </div>
             </div>
             <Separator className="" />
             <div>
-              <h5>Vehicules en intervention</h5>
+              <div className=" flex flex-row items-center gap-3">
+                <TbFiretruck />
+                <h5>Vehicules en intervention :</h5>
+              </div>
               <div>
                 {intervention[0].vehicules.map((doc) => (
-                  <div key={doc.id}>{doc.name}</div>
+                  <div
+                    className=" bg-red-600 w-32 flex justify-center rounded-sm text-white"
+                    key={doc.id}
+                  >
+                    {doc.name}
+                  </div>
                 ))}
               </div>
             </div>
--- components/game/communication.tsx
+++ components/game/communication.tsx
@@ -2,6 +2,9 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { PrismaClient } from "@prisma/client";
+import { BsFillTelephoneOutboundFill } from "react-icons/bs";
+import { FaWalkieTalkie } from "react-icons/fa6";
+import { GiNothingToSay } from "react-icons/gi";
 import { GenerationInter } from "./generation-inter";
 import { Radio } from "./radio";
 
@@ -24,6 +27,53 @@ const Communication: React.FC<Props> = async () => {
         <CardContent className=" flex flex-col gap-6">
           <GenerationInter intervention={intervention} commune={commune} />
           <Radio generation={generation} />
+          <div>
+            <h2>Appel provenant du 18/112</h2>
+            <div className=" w-full h-10 border-2 border-black rounded-sm flex justify-center items-center">
+              <GiNothingToSay />
+            </div>
+          </div>
+          <div>
+            <h2>Appel provenant du samu</h2>
+            <div className=" w-full h-10 border-2 border-black rounded-sm flex justify-center items-center bg-red-600">
+              <BsFillTelephoneOutboundFill />
+            </div>
+          </div>
+          <div>
+            <h2>Appel provenant de la gendarmerie/police</h2>
+            <div className=" w-full h-10 border-2 border-black rounded-sm flex justify-center items-center">
+              <GiNothingToSay />
+            </div>
+          </div>
+          <div>
+            <h2>Appel provenant d&apos;une caserne ou d&apos;un sapeur</h2>
+            <div className=" w-full h-10 border-2 border-black rounded-sm flex justify-center items-center">
+              <GiNothingToSay />
+            </div>
+          </div>
+          <div>
+            <h2>Demande de parole radio</h2>
+            <div className=" w-full h-10 border-2 border-black rounded-sm flex justify-center items-center bg-red-600">
+              <FaWalkieTalkie />
+            </div>
+          </div>
+          <div>
+            <h2>Telephone</h2>
+            <div className=" w-full border-2 border-black rounded-sm">
+              <div className="w-full h-10 border-2 border-gray-500 rounded-sm"></div>
+              <div className=" grid grid-cols-3 gap-4">
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+              </div>
+            </div>
+          </div>
         </CardContent>
       </Card>
     </div>
